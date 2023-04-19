ff15fa4e800bf0d7e5cfb19332e0ea7017811a9e
Gleb <vanechka_samoylov_90@mail.ru>
2023-04-19 20:33:56 +0300

    create Block and init Main

---
+++ block.py
@@ -0,0 +1,17 @@
+import hashlib
+
+
+class Block:
+    def __init__(self, index, prev_hash, block_hash, data, nonce):
+        self.index = index
+        self.prev_hash = prev_hash
+        self.hash = block_hash
+        self.data = data
+        self.nonce = nonce
+
+    def check_hash(self):
+        return hashlib.sha256((str(self.index) + self.prev_hash + self.data + str(self.nonce)).encode(
+            'utf-8')).hexdigest() == self.hash
+
+    def __str__(self):
+        return "index = {} ; prev_hash = {} ; hash = {}".format(self.index, self.prev_hash, self.hash)
